f7b70bbac85bfc81f2cb91a74d69a7db6b58a104
ioanabirsan <ioana.c.birsan@gmail.com>
2018-12-17 23:16:34 +0200

    Add Swagger support

--- clms/Notifications.API/Startup.cs
+++ clms/Notifications.API/Startup.cs
@@ -24,6 +24,19 @@ namespace Notifications.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "clms - Notifications HTTP API",
+                    Version = "v1",
+                    Description = "The Notifications Microservice HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -34,6 +47,12 @@ namespace Notifications.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notifications.API");
+                });
 
             app.UseMvc();
         }
--- clms/Users.API/Startup.cs
+++ clms/Users.API/Startup.cs
@@ -24,7 +24,24 @@ namespace Users.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "clms - Users HTTP API",
+                    Version = "v1",
+                    Description = "The Users Microservice HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+            });
+
+//            services.AddDbContext<ApplicationContext>(options => options.UseMySql(@"server=fenrir.info.uaic.ro;uid=clmsusers;pwd=QEtCDIZR6t;database=clmsusers"));
+//            services.AddTransient<IUserRepository, UserRepository>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +51,12 @@ namespace Users.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users.API");
+                });
 
             app.UseMvc();
         }
