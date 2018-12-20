701fc04dadf3552bef4915d18d90fcf0761fa056
Andrada Ciulei <andradaciulei@yahoo.com>
2018-12-20 21:53:12 +0200

    added ReadTeacherRepository and WriteTeacherRepository implementation

--- clms/Users.API/Repository/Write/WriteTeacherRepository.cs
+++ clms/Users.API/Repository/Write/WriteTeacherRepository.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using Users.API.Models;
+using System.Linq;
+using Users.API.Context;
 
 namespace Users.API.Repository.Write
 {
     public class WriteTeacherRepository : IWriteRepository<Teacher>
     {
-        public void Create(Teacher user)
+        private readonly ApplicationContext _context;
+
+        public WriteTeacherRepository(ApplicationContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public void Create(Teacher teacher)
+        {
+            _context.Teachers.Add(teacher);
         }
 
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            Teacher teacher = _context.Teachers.First(p => p.Id == id);
+            _context.Teachers.Remove(teacher);
         }
 
         public bool Exists(Guid id)
         {
-            throw new NotImplementedException();
+            return _context.Teachers.Any(p => p.Id == id);
         }
 
         public void SaveChanges()
         {
-            throw new NotImplementedException();
+            _context.SaveChanges();
         }
     }
 }
