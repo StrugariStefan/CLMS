6b6a6962df592634cbd22c74458dce6522b3fb4e
ioanabirsan <ioana.c.birsan@gmail.com>
2018-12-20 17:55:03 +0200

    Add initial model validation

---
+++ clms/Users.API/Helpers/UserDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Users.API.Models;
+
+namespace Users.API.Helpers
+{
+//    https://cecilphillip.com/fluent-validation-rules-with-asp-net-core/
+    public class UserDtoValidator : AbstractValidator<UserDto>
+    {
+        public UserDtoValidator()
+        {
+            RuleFor(user => user.Name).NotNull().Matches("[A-Z][a-z'-]+[ ][A-Z.][ ][A-Z][a-z'-]");
+            RuleFor(user => user.Phone).NotNull().Matches("(00|\\+)40\\d{10}");
+            RuleFor(user => user.Email).NotNull().Matches("[a-zA-Z0-9_.-]+@[a-z]+.[a-z]+");
+        }
+    }
+}
