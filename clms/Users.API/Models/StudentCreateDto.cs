0a87aadd13f820ac4fe6639c824080cf287aaa9f
Andrada Ciulei <andradaciulei@yahoo.com>
2018-12-21 00:04:31 +0200

    updated StudentsController

--- clms/Users.API/Models/StudentCreateDto.cs
+++ clms/Users.API/Models/StudentCreateDto.cs
@@ -5,13 +5,12 @@ using System.Threading.Tasks;
 
 namespace Users.API.Models
 {
-    public class StudentDto : UserDto
+    public class StudentCreateDto:UserCreateDto
     {
         //public IEnumerable<String> EnrolledClasses { get; set; }
         //public IEnumerable<int> Grades { get; set; }
         public int Year { get; set; }
         public string Group { get; set; }
         public string StudyProgram { get; set; }
-
     }
 }
