956962feebb604fc5364ff1a0219cfd596e96b8e
StrugariStefan <mihaistrugari@yahoo.com>
2019-01-04 05:45:18 +0200

    added AuthFilter and updated course controller response description

--- clms/Courses.API/Controllers/CoursesController.cs
+++ clms/Courses.API/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Courses.API.Filters;
 using Courses.API.Helpers;
 using Courses.API.Models;
 using Courses.API.Repository.Read;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Courses.API.Controllers
 {
+    [Produces("application / json")]
     [Route("api/v1/courses")]
     [ApiController]
     public class CoursesController : ControllerBase
@@ -27,6 +29,8 @@ namespace Courses.API.Controllers
         /// Return all courses.
         /// </summary>
         [HttpGet]
+        [AuthFilter]
+        [ProducesResponseType(200)]
         public ActionResult<IReadOnlyList<CourseDto>> Get()
         {
             return Ok(_mapper.EntityCollectionToDtoCollection(_readCourseRepository.GetAll()));
@@ -35,7 +39,13 @@ namespace Courses.API.Controllers
         /// <summary>
         /// Obtains course by id.
         /// </summary>
+        /// <param name="id"></param>
+        /// <response code="200">Specified course</response>
+        /// <response code="404">If course id doesn't exists</response>
         [HttpGet("{id}", Name = "GetByCourseId")]
+        [AuthFilter]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
         public ActionResult<CourseDto> Get(Guid id)
         {
             if (!_readCourseRepository.Exists(id))
@@ -49,7 +59,13 @@ namespace Courses.API.Controllers
         /// <summary>
         /// Obtains course by name.
         /// </summary>
+        /// <param name="name"></param>
+        /// <response code="200">Specified course</response>
+        /// <response code="404">If course name doesn't exists</response>
         [HttpGet("{name}", Name = "GetByCourseName")]
+        [AuthFilter]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
         public ActionResult<CourseDto> GetByName(string name)
         {
             Course course = _readCourseRepository.GetByName(name);
@@ -65,7 +81,12 @@ namespace Courses.API.Controllers
         /// <summary>
         /// Creates a new course.
         /// </summary>
+        /// <response code="201">The created course</response>
+        /// <response code="400">If courseDto is null, model is not valid or name already exists</response>
         [HttpPost]
+        [AuthFilter]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
         public ActionResult<Course> Post([FromBody] CourseCreateDto courseCreateDto)
         {
             if (courseCreateDto == null)
@@ -94,7 +115,13 @@ namespace Courses.API.Controllers
         /// <summary>
         /// Deletes a specific Course.
         /// </summary>
+        /// <param name="id"></param>
+        /// <response code="204">Course has been deleted</response>
+        /// <response code="404">If course id is not found</response>
         [HttpDelete("{id}")]
+        [AuthFilter]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
         public ActionResult<Course> Delete(Guid id)
         {
             if (!_writeCourseRepository.Exists(id))
