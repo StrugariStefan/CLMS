b9d8cf60245cf7cf4488ccf6de6efc4ae00ae39d
SilistruAlexandru <silistrualexandru@gmail.com>
2019-01-13 02:49:16 +0200

    Added a first version of Gamification validators

---
+++ clms/Gamification.API/Validators/AnswerValidator.cs
@@ -0,0 +1,13 @@
+﻿using Gamification.API.Models;
+using FluentValidation;
+
+namespace Gamification.API.Validators
+{
+    public class AnswerValidator : AbstractValidator<Answer>
+    {
+        public AnswerValidator()
+        {
+            RuleFor(answer => answer.ActualAnswer).NotEmpty();
+        }
+    }
+}
